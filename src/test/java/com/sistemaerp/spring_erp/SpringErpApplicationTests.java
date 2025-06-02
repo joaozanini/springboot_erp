6f56d41ead429271b3314232e62b0ff348c4a272
João Vitor <joaozanini.profissional@gmail.com>
2025-06-01 22:36:45 -0300

    spring

---
+++ src/test/java/com/sistemaerp/spring_erp/SpringErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaerp.spring_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
