e11f400da2f4e191eb33e66d5811b2c195c089d6
João Zanini <joaozanini.pessoal@gmail.com>
2026-03-19 19:40:26 -0300

    .

---
+++ api/src/main/java/com/joaozanini/erp/ErpSysApplication.java
@@ -0,0 +1,13 @@
+package com.joaozanini.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSysApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/joaozanini/erp/ErpSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaozanini.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
