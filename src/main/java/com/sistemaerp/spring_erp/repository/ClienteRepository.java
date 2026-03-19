9f3cc604550e43e9fe73a252da7252b87cb299b3
João Zanini <joaozanini.pessoal@gmail.com>
2026-03-19 19:11:07 -0300

    .

--- src/main/java/com/sistemaerp/spring_erp/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistemaerp.spring_erp.repository;
-
-import com.sistemaerp.spring_erp.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- src/main/java/com/sistemaerp/spring_erp/repository/VendaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sistemaerp.spring_erp.repository;
-
-import com.sistemaerp.spring_erp.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendaRepository extends JpaRepository<Venda, Long> {
-
-}
