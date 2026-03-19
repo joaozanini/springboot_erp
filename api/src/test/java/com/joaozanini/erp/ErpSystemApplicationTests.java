db4d1a4643e720a15c399183fd320533f6540b12
João Zanini <joaozanini.pessoal@gmail.com>
2026-03-19 19:23:13 -0300

    springboot setup

---
+++ api/src/test/java/com/joaozanini/erp/ErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaozanini.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
